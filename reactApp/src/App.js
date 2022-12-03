037781f09fc4ae86740179da9e017f5be0b2d1f6
polyacrilamide <78802419+polyacrilamide@users.noreply.github.com>
2022-12-03 17:46:49 +0300

    Still learning things

--- reactApp/src/App.js
+++ reactApp/src/App.js
@@ -9,23 +9,11 @@ import Ccomponent from './Ccomponent';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-        <Ccomponent number ="123"/>
-  
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">  
+      <Ccomponent />
+
       </header>
+      
     </div>
   );
 }
