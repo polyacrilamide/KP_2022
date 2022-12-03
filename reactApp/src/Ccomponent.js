4ab3b37cb66068f0f6a95f9a1dbb71ff7419b9c1
polyacrilamide <78802419+polyacrilamide@users.noreply.github.com>
2022-12-03 17:47:17 +0300

    Still learning things

--- reactApp/src/Ccomponent.js
+++ reactApp/src/Ccomponent.js
@@ -1,42 +1,61 @@
 import React, {Component} from 'react'
+import './styles.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+
+
+
 
 export default class Ccomponent extends Component{
     constructor(props){
-        super(props) 
+        super(props); 
 
         this.state = {
-            count: 0
-        }
+            input:"",
+            items: []
+        };
 
-        this.clickIncrement = this.clickIncrement.bind(this);
-        this.clickDecrement = this.clickDecrement.bind(this);
-        this.clickReset = this.clickReset.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
     }
     //стейт обновлять через странную стрелочку
-    clickIncrement(){
-        this.setState(state => ({ count: state.count + 1 
-        }));
-    }
-
-    clickDecrement(){
-        this.setState(state =>({ count: state.count - 1
-        }))
-    }
-
-    clickReset(){
+    // clickIncrement(){
+    //     this.setState(state => ({ count: state.count + 1 
+    //     }));
+    // }
+    handleChange(event){
         this.setState({
-            count: 0
+            input: event.target.value
         })
     }
 
+    handleSubmit(event){
+        event.preventDefault()
+        if(this.state.input !=""){
+            this.setState({
+                input: this.state.input,
+                items: [...this.state.items, this.state.input],
+                input: ""
+            })
+        }
+    }
+
+   
     render(){
         return (
             <div>
-                <h1>{this.state.count}</h1>
-                <button onClick={this.clickIncrement}>Добавить 1</button>
-                <button onClick={this.clickDecrement}>Отнять 1</button>
-                <button onClick={this.clickReset}>Очистить</button>
+                <Stack direction ="column" spacing={2}>
+                    <TextField id="outlined-basic" label="Введите что-нибудь" variant="outlined" value = {this.state.input} onChange = {this.handleChange} />
+                    <Button variant="contained" onClick ={this.handleSubmit}>Подтвердить</Button>
+                </Stack>
+                <ul>
+                    {this.state.items.map((item, index) =>(
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
             </div>
-        )
+        );
     }
 }
